35bae168473423bfcdf96850e07a60360b7e5590
xl_g <iticomilk@outlook.com>
2022-12-08 09:55:33 +0900

    [add] encrypt and decrypt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,97 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Result};
+use blake2::digest::{Update, VariableOutput};
+use blake2::Blake2bVar;
+use chacha20poly1305::KeyInit;
+use chacha20poly1305::{aead::stream, XChaCha20Poly1305};
+use clap::{arg, Command};
+use std::path::Path;
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
+
+pub fn encrypt_file<P: AsRef<Path>>(source: P, dist: P, token: &[u8]) -> Result<()> {
+    // init encryptor
+    let xkey: &[u8; 32] = token[..32].try_into().unwrap();
+    let xnonce: &[u8; 19] = token[32..51].try_into().unwrap();
+    let aead = XChaCha20Poly1305::new(xkey.as_ref().into());
+    let mut stream_encryptor = stream::EncryptorBE32::from_aead(aead, xnonce.as_ref().into());
+    // encrypt each part
+    const BUFFER_LEN: usize = 500;
+    let mut buffer = [0u8; BUFFER_LEN];
+    let mut source_file = File::open(source)?;
+    let mut dist_file = File::create(dist)?;
+    loop {
+        let read_count = source_file.read(&mut buffer)?;
+        if read_count == BUFFER_LEN {
+            let cipher_text = stream_encryptor
+                .encrypt_next(buffer.as_slice())
+                .map_err(|err| anyhow!("Encrypting file: {}", err))?;
+            dist_file.write_all(&cipher_text)?;
+        } else {
+            let cipher_text = stream_encryptor
+                .encrypt_last(&buffer[..read_count])
+                .map_err(|err| anyhow!("Encrypting file: {}", err))?;
+            dist_file.write_all(&cipher_text)?;
+            break;
+        }
+    }
+    Ok(())
+}
+
+pub fn decrypt_file<P: AsRef<Path>>(source: P, dist: P, token: &[u8]) -> Result<()> {
+    // init encryptor
+    let xkey: &[u8; 32] = token[..32].try_into().unwrap();
+    let xnonce: &[u8; 19] = token[32..51].try_into().unwrap();
+    let aead = XChaCha20Poly1305::new(xkey.as_ref().into());
+    let mut stream_decryptor = stream::DecryptorBE32::from_aead(aead, xnonce.as_ref().into());
+    const BUFFER_LEN: usize = 500 + 16;
+    let mut buffer = [0u8; BUFFER_LEN];
+    let mut encrypted_file = File::open(source)?;
+    let mut dist_file = File::create(dist)?;
+    loop {
+        let read_count = encrypted_file.read(&mut buffer)?;
+        if read_count == BUFFER_LEN {
+            let plain_text = stream_decryptor
+                .decrypt_next(buffer.as_slice())
+                .map_err(|err| anyhow!("Decrypting file: {}", err))?;
+            dist_file.write_all(&plain_text)?;
+        } else if read_count == 0 {
+            break;
+        } else {
+            let plain_text = stream_decryptor
+                .decrypt_last(&buffer[..read_count])
+                .map_err(|err| anyhow!("Decrypting file: {}", err))?;
+            dist_file.write_all(&plain_text)?;
+            break;
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let matches = Command::new("crab")
+        .version("0.1")
+        .author("xl_g <lr_kkr@outlook.com>")
+        .about("A file cryptor")
+        .arg(arg!(-e --encrypt <Token> "Encrypt dir").required(false))
+        .arg(arg!(-d --decrypt <Token> "Decrypt dir").required(false))
+        .get_matches();
+    if let Some(token) = matches.get_one::<String>("encrypt") {
+        println!("Value for name: {}", token);
+        let mut hasher = Blake2bVar::new(51).unwrap();
+        hasher.update(token.as_bytes());
+        let mut buf = [0u8; 51];
+        hasher.finalize_variable(&mut buf).unwrap();
+        encrypt_file("test.txt", "test.txt.crab", &buf)?;
+    }
+    if let Some(token) = matches.get_one::<String>("decrypt") {
+        println!("Value for name: {}", token);
+        let mut hasher = Blake2bVar::new(51).unwrap();
+        hasher.update(token.as_bytes());
+        let mut buf = [0u8; 51];
+        hasher.finalize_variable(&mut buf).unwrap();
+        decrypt_file("test.txt.crab", "test.txt", &buf)?;
+    }
+    Ok(())
 }
