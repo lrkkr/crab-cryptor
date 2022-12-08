b821db78fcd53760de5c6aa3af80d7d06a650e0a
xl_g <iticomilk@outlook.com>
2022-12-08 11:36:14 +0900

    [add] crypt text

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 use anyhow::{anyhow, Result};
 use blake2::digest::{Update, VariableOutput};
 use blake2::Blake2bVar;
-use chacha20poly1305::KeyInit;
-use chacha20poly1305::{aead::stream, XChaCha20Poly1305};
+use chacha20poly1305::{
+    aead::{stream, Aead},
+    KeyInit, XChaCha20Poly1305,
+};
 use clap::{arg, Command};
 use std::path::Path;
 use std::{
     fs::File,
     io::{Read, Write},
 };
+use walkdir::WalkDir;
+
+pub fn encrypt(plain_text: String, token: &[u8]) -> Result<String> {
+    // init encryptor
+    let xkey: &[u8; 32] = token[..32].try_into().unwrap();
+    let xnonce: &[u8; 24] = token[27..51].try_into().unwrap();
+    let cipher = XChaCha20Poly1305::new(xkey.as_ref().into());
+    // encrypt
+    let cipher_text = cipher
+        .encrypt(xnonce[..].into(), plain_text.as_bytes())
+        .map_err(|err| anyhow!("Encrypting text: {}", err))?;
+    Ok(base64::encode(&cipher_text))
+}
+
+pub fn decrypt(cipher_text: String, token: &[u8]) -> Result<String> {
+    // init encryptor
+    let xkey: &[u8; 32] = token[..32].try_into().unwrap();
+    let xnonce: &[u8; 24] = token[27..51].try_into().unwrap();
+    let cipher = XChaCha20Poly1305::new(xkey.as_ref().into());
+    // decrypt
+    let plain_text = cipher
+        .decrypt(xnonce[..].into(), cipher_text.as_bytes())
+        .map_err(|err| anyhow!("Decrypting text: {}", err))?;
+    Ok(base64::encode(&plain_text))
+}
 
 pub fn encrypt_file<P: AsRef<Path>>(source: P, dist: P, token: &[u8]) -> Result<()> {
     // init encryptor
@@ -78,20 +105,51 @@ fn main() -> Result<()> {
         .arg(arg!(-d --decrypt <Token> "Decrypt dir").required(false))
         .get_matches();
     if let Some(token) = matches.get_one::<String>("encrypt") {
-        println!("Value for name: {}", token);
+        // extend token
         let mut hasher = Blake2bVar::new(51).unwrap();
         hasher.update(token.as_bytes());
         let mut buf = [0u8; 51];
         hasher.finalize_variable(&mut buf).unwrap();
-        encrypt_file("test.txt", "test.txt.crab", &buf)?;
+        // walk dir
+        for entry in WalkDir::new("./").into_iter() {
+            let entry = entry?;
+            if entry.metadata()?.is_file() {
+                let source = entry.path();
+                println!("{}", source.display());
+            }
+        }
+        // encrypt_file("test.txt", "test.txt.crab", &buf)?;
     }
     if let Some(token) = matches.get_one::<String>("decrypt") {
-        println!("Value for name: {}", token);
+        // extend token
         let mut hasher = Blake2bVar::new(51).unwrap();
         hasher.update(token.as_bytes());
         let mut buf = [0u8; 51];
         hasher.finalize_variable(&mut buf).unwrap();
+        // walk dir
         decrypt_file("test.txt.crab", "test.txt", &buf)?;
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use blake2::digest::{Update, VariableOutput};
+    use blake2::Blake2bVar;
+
+    use crate::encrypt;
+
+    #[test]
+    fn crypt_test() {
+        let token = String::from("crab");
+        let mut hasher = Blake2bVar::new(51).unwrap();
+        hasher.update(token.as_bytes());
+        let mut buf = [0u8; 51];
+        hasher.finalize_variable(&mut buf).unwrap();
+        let cipher_text = encrypt("plain_text".to_owned(), &buf).unwrap();
+        assert_eq!(
+            cipher_text,
+            "zGEeAhLTe+2D7lkYnP1fLL9e67L8aEqrJ94=".to_owned()
+        );
+    }
 }
